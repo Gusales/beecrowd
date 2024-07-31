63a2d07661ec59943f8609f5332142c5b0bc0fd8
Gusales <dev.gussales@gmail.com>
2024-07-31 15:16:50 -0300

    feat: upload challenge 1007

---
+++ resolutions_in_java/Main1007.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main1007 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("DIFERENCA = %d\n", ((input.nextInt() * input.nextInt()) - (input.nextInt() * input.nextInt())));
+    }
+}
