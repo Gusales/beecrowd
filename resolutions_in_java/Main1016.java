304b849fba61a6b0a097392bfcd0833d2431ab2c
Gusales <dev.gussales@gmail.com>
2024-08-02 12:00:31 -0300

    submit problem 1016

---
+++ resolutions_in_java/Main1016.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main1016 {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int distance = input.nextInt();
+        System.out.printf("%d minutos", distance * 2);
+    }
+}
